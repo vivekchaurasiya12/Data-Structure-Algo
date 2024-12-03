c3cce809f9df3a14039318e562606446a2dbc3b8
vivekchaurasiya12 <132990592+vivekchaurasiya12@users.noreply.github.com>
2024-12-03 20:15:22 +0530

    Daily DSA progress - 03-12-2024

--- String/checkAnagram.cpp
+++ String/checkAnagram.cpp
@@ -7,14 +7,30 @@ bool isAnagram(string s, string t) {
     if(s.size() != t.size()) {
         return false;
     }
-
     // Sort both strings
     sort(s.begin(), s.end());
     sort(t.begin(), t.end());
 
     // After sorting, if both strings are the same, they are anagrams
     return s == t;
+
+    
 }
+// Optimal approach for chekcing anagram 
+
+// bool checkAnagram(string str1,string str2){
+//      int freq[26] = {0};
+//      for(int i=0;i<str1.length();i++){
+//         freq[str1[i]-'a']++;
+//      }
+//      for(int i=0;i<str2.length();i++){
+//         freq[str2[i]-'a']--;
+//      }
+//     for(int i=0;i<26;i++){
+//         if(freq[i]!=0) return false;
+//     }
+//      return true;
+// }
 
 int main() {
     string s = "listen";
